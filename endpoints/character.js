6819b1a5e46868922a87006fd994f92817dfba58
Arnout Kazemier <info@3rd-Eden.com>
2016-12-16 23:39:25 +0100

    [fix] Actually store destiny instance [api] Added advisors API [api] Added activity API

--- endpoints/character.js
+++ endpoints/character.js
@@ -7,7 +7,49 @@
  */
 export default class Character {
   constructor(destiny) {
-    this.destiny;
+    this.destiny = destiny;
+  }
+
+  /**
+   * Retrieve advisors for a given character.
+   *
+   * @param {String|Number} platform The platform type.
+   * @param {String} id Destiny id
+   * @param {string} char Character id.
+   * @param {Function} fn Completion callback.
+   * @public
+   */
+  advisors(platform, id, char, fn) {
+    return this.destiny.send({
+      url: 'Destiny/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Advisors/V2/',
+      filter: 'data',
+      format: {
+        membershipType: this.destiny.console(platform),
+        destinyMembershipId: id,
+        characterId: char
+      }
+    }, fn);
+  }
+
+  /**
+   * Retrieve activities for a given character.
+   *
+   * @param {String|Number} platform The platform type.
+   * @param {String} id Destiny id
+   * @param {string} char Character id.
+   * @param {Function} fn Completion callback.
+   * @public
+   */
+  activities(platform, id, char, fn) {
+    return this.destiny.send({
+      url: 'Destiny/{membershipType}/Account/{destinyMembershipId}/Character/{characterId}/Activities/',
+      filter: 'data',
+      format: {
+        membershipType: this.destiny.console(platform),
+        destinyMembershipId: id,
+        characterId: char
+      }
+    }, fn);
   }
 
   /**
@@ -49,25 +91,4 @@ export default class Character {
       }
     }, fn);
   }
-
-  /**
-   * Retrieve activity history for a given character.
-   *
-   * @param {String|Number} platform The platform type.
-   * @param {String} id Destiny id
-   * @param {string} char Character id.
-   * @param {Function} fn Completion callback.
-   * @public
-   */
-  history(platform, id, char, fn) {
-    return this.destiny.send({
-      url: 'Destiny/Stats/ActivityHistory/{membershipType}/{destinyMembershipId}/{characterId}/',
-      format: {
-        membershipType: this.destiny.console(platform),
-        destinyMembershipId: id,
-        characterId: char
-      }
-    }, fn);
-  }
-
 }
